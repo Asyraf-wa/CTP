4685cbb8426953f93fa5bc4e292a39e67c6c9a20
Asyraf <asyraf.wahianuar@gmail.com>
2021-11-27 07:59:10 +0800

    Fav icon update

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -23,7 +23,15 @@ $a_name = $this->request->getParam('action');
 		?>
 		<?= $this->Html->meta('copyright', $meta_copyright, ['block' => true]); ?>
 		<?php //echo $this->Html->meta('icon') ?>
-		<?php echo $this->Html->meta('favicon.ico','/favicon.ico',['type' => 'icon']); ?>
+		<?php //echo $this->Html->meta('favicon.ico','/favicon.ico',['type' => 'icon']); ?>
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1">
+<link rel="manifest" href="/site.webmanifest?v=1">
+<link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#5bbad5">
+<link rel="shortcut icon" href="/favicon.ico?v=1">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
 		<?= $this->fetch('meta') ?>
 	<?php echo $this->Html->css('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css') ?>
 	<?php echo $this->Html->css('combine-dark-custom.min.css') ?>
