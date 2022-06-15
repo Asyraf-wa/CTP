e91515698b910a4d3be0af861ce88c96eff33ffd
Asyraf <asyraf.wahianuar@gmail.com>
2022-06-15 16:04:51 +0800

    Update CSS minify

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -39,7 +39,7 @@ if ('serviceWorker' in navigator) {
 <meta name="theme-color" content="#ffffff">
 		<?= $this->fetch('meta') ?>
 	<?php echo $this->Html->css('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css') ?>
-	<?php echo $this->Html->css('combine-dark-custom.min.css') ?>
+	<?php echo $this->Html->css('combine-dark-custom-v1.min') ?>
 	<?php //echo $this->Html->css('dark.min.css') ?>
 	<?php //echo $this->Html->css(['custom.min.css']) ?>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
