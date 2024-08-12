c72626ec9687a1a9b062bc20451656f2cbca5206
Asyraf <asyraf.wahianuar@gmail.com>
2024-08-12 21:03:14 +0800

    Delete Everything

--- templates/Pages/about.php
+++
@@ -1,7 +0,0 @@
-<div class="container pt-5"><!--start container-->
-	<h4 class="fw-bold">ABOUT</h4>
-	<hr>
-	<div class="text-center">
-	<b class="gradient-animate">&lt;&#47;&gt; Code The Pixel</b>
-	</div>
-</div><!--end container-->
--- templates/layout/sitemap.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-echo $this->fetch('content');
-echo '</urlset>';
-?>
