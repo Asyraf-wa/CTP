631d453b224f5f800e3f070aa5346ea21dbc8ae9
Asyraf <asyraf.wahianuar@gmail.com>
2024-11-07 09:09:49 +0800

    update favicon

--- templates/layout/articles.php
+++ templates/layout/articles.php
@@ -49,6 +49,8 @@ $a_name = $this->request->getParam('action');
 
     <title><?= $system_abbr ?>: <?= $this->fetch('title') ?></title>
     <?= $this->Html->meta('icon') ?>
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
     <link rel="shortcut icon" href="/favicon.ico" />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     <meta name="apple-mobile-web-app-title" content="Code The Pixel" />
