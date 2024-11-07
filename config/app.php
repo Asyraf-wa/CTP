4f87b3277284d380067ef14ad0a9db635669acef
Asyraf <asyraf.wahianuar@gmail.com>
2024-11-07 12:31:13 +0800

    config session

--- config/app.php
+++ config/app.php
@@ -406,7 +406,7 @@ return [
      * To use database sessions, load the SQL file located at config/schema/sessions.sql
      */
     'Session' => [
-        'defaults' => 'php',
+        'defaults' => 'database',
     ],
     'AuditStash' => [
         'persister' => 'AuditStash\Persister\TablePersister'
