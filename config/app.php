9cabc4f1dde66d31f5b0307aef93a42d1055826d
Asyraf <asyraf.wahianuar@gmail.com>
2024-11-07 15:58:27 +0800

    deprecation

--- config/app.php
+++ config/app.php
@@ -171,7 +171,7 @@ return [
      *   your application that still emit deprecations.
      */
     'Error' => [
-        'errorLevel' => E_ALL & ~E_USER_DEPRECATED,
+        'errorLevel' => E_ALL ^ E_DEPRECATED ^ E_USER_DEPRECATED,
         'skipLog' => [],
         'log' => true,
         'trace' => true,
